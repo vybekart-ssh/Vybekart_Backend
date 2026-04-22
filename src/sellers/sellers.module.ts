9351405b3b6dd809c5d2144175bc3418bc721c95
Vishal Ugalmugale <vybekart88@gmail.com>
2026-04-22 12:11:07 +0530

    removed render.yaml secret keys

--- src/sellers/sellers.module.ts
+++ src/sellers/sellers.module.ts
@@ -3,9 +3,10 @@ import { SellersService } from './sellers.service';
 import { SellersController } from './sellers.controller';
 import { PrismaModule } from '../prisma/prisma.module';
 import { AuthModule } from '../auth/auth.module';
+import { StorageModule } from '../storage/storage.module';
 
 @Module({
-  imports: [PrismaModule, AuthModule],
+  imports: [PrismaModule, AuthModule, StorageModule],
   controllers: [SellersController],
   providers: [SellersService],
   exports: [SellersService],
